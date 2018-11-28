3377e63d5f4c001b0af0d1fff96c97acbce69b8e
Abraham Bueno <abrahambueno@localhost.localdomain>
2018-11-28 17:20:13 -0500

    Testing github

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -8,8 +8,9 @@ beforeEach(() => {
 });
 
 afterEach(() => {
-    wrapped.unmount();
-})
+  wrapped.unmount();
+  console.log("lol");
+});
 it("has a text area and a button", () => {
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
