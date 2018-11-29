4d2441e0c3e09ab4d82b1555ecc2579f61ab412d
Abraham Bueno <abueno@localhost.localdomain>
2018-11-29 16:53:20 -0600

    The submit button test are written and passing.

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -22,3 +22,13 @@ it("has a text area that users can type in", () => {
   wrapped.update();
   expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
 });
+
+it("when submitting form, text area gets emptied", () => {
+  wrapped.find("textarea").simulate("change", {
+    target: { value: "New CommeNt" }
+  });
+  wrapped.update();
+  wrapped.find("form").simulate("submit");
+  wrapped.update();
+  expect(wrapped.find("textarea").prop("value")).toEqual("");
+});
