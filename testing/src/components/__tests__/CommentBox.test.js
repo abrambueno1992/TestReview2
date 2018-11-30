499a5131ad126314cbf133015016114a76997452
Abraham Bueno <abueno@localhost.localdomain>
2018-11-29 23:59:38 -0600

    Added expect to better format

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -14,21 +14,20 @@ it("has a text area and a button", () => {
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
 });
-
-it("has a text area that users can type in", () => {
-  wrapped.find("textarea").simulate("change", {
-    target: { value: "new comment" }
+describe("the text area", () => {
+  beforeEach(() => {
+    wrapped.find("textarea").simulate("change", {
+      target: { value: "New CommeNt" }
+    });
+    wrapped.update();
   });
-  wrapped.update();
-  expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
-});
-
-it("when submitting form, text area gets emptied", () => {
-  wrapped.find("textarea").simulate("change", {
-    target: { value: "New CommeNt" }
+  it("has a text area that users can type in", () => {
+    expect(wrapped.find("textarea").prop("value")).toEqual("New CommeNt");
   });
-  wrapped.update();
-  wrapped.find("form").simulate("submit");
-  wrapped.update();
-  expect(wrapped.find("textarea").prop("value")).toEqual("");
+
+  it("when submitting form, text area gets emptied", () => {
+    wrapped.find("form").simulate("submit");
+    wrapped.update();
+    expect(wrapped.find("textarea").prop("value")).toEqual("");
+  });
 });
